2feaac0107657885c9ca4ca0227243dd5898fce9
Peter Jang <peter.x.jang@gmail.com>
2020-08-30 10:39:51 -0500

    Use a view to display message and current time

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,5 +1,8 @@
 class Api::ExamplePagesController < ApplicationController
   def hello_method
-    render json: { message: "Hello!" }
+    # render json: { message: "Hello!", time: Time.now.strftime("%d/%m/%Y %H:%M") }
+    @message = "Hello!"
+    @time = Time.now.strftime("%d/%m/%Y %H:%M")
+    render "hello.json.jb"
   end
 end
